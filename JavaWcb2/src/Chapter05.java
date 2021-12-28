513998e572624a3df819c744d83ebc84a9e56862
suzuya0 <sakura240613@gmail.com>
2021-12-28 17:42:22 +0900

    [Fisnih]basic_ch5

---
+++ JavaWcb2/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long num1 = 5L;
+		System.out.println(num1);
+		double num2 = 6.3;
+		System.out.println(num2);
+		boolean bool = true;
+		System.out.println(bool);
+		Float num3 = 3.2f;
+		System.out.println(num3);
+		Integer num4 = 7;
+		System.out.println(num4);
+		Character str = 'う';
+		System.out.println(str);
+		int[] int1 = { 4, 67, 2, 1, 4};
+		System.out.println(int1[3]);
+		
+	}
+
+}
