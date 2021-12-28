7718b88248204124aff6b7ae91c55f558a575f7f
suzuya0 <sakura240613@gmail.com>
2021-12-28 15:41:27 +0900

    [Fisnih]basic_ch4

---
+++ JavaWcb2/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "yamada";
+		System.out.println(name);
+		name = "suzuki";
+		System.out.println(name);
+		final int LUCCKY_NUMBER = 7;
+		System.out.println(LUCCKY_NUMBER);
+		
+	}
+}
